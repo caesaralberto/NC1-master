c5b8470ac1fcccf4df867a3422fd60a649941400
caesar <caesaralbertofernandez@gmail.com>
2019-05-23 02:20:03 +0700

    fix launch screen background

--- Funion/Splash Screen/SplashScreenViewController.swift
+++ Funion/Splash Screen/SplashScreenViewController.swift
@@ -13,6 +13,12 @@ class SplashScreenViewController: UIViewController {
     @IBOutlet weak var splashScreenImage: UIImageView!
     override func viewDidAppear(_ animated: Bool) {
         super .viewDidAppear(animated)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.init().shadeOrange.cgColor, UIColor.init().middleOrange.cgColor, UIColor.init().tintOrange.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5,y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5,y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
         
         UIView.animate(withDuration: 1, delay: 0.7, options: [.curveEaseInOut], animations: {
             let rotate = CGAffineTransform(rotationAngle: 45 * 3)
